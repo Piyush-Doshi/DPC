62e0bc937b248cf3c02fdeb70016df6b056918cb
Piyush-Doshi <piyush3969004@gmail.com>
2025-08-15 21:46:27 +0530

    day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,27 +1,26 @@
+/*Sort an Array of 0s, 1s, and 2s You are given an array arr consisting only of 0s, 1s, and 2s. The task is to sort the array in increasing order in linear time (i.e., O(n)) without using any extra space. This means you need to rearrange the array in-place.
+*/
 #include <iostream>
 #include <vector>
-#include <algorithm> // for sort()
-
+#include <algorithm> 
 using namespace std;
 
 int main() {
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
-
     vector<int> arr(n);
     cout << "Enter " << n << " elements (only 0, 1, or 2): ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
-    sort(arr.begin(), arr.end()); // Sorting in increasing order
-
+    
+    sort(arr.begin(), arr.end()); 
     cout << "Sorted array: ";
     for (int num : arr) {
         cout << num << " ";
     }
     cout << endl;
-
     return 0;
 }
+
