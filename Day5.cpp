1d56c09e65f413101c3d58d21eaeb0444bd89753
Piyush-Doshi <piyush3969004@gmail.com>
2025-08-19 20:33:30 +0530

    Add files via upload

---
+++ Day5.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+  
+vector<int> findLeaders(const vector<int>&arr){
+    int n=arr.size();
+    vector<int> leaders;
+    int maxRight=arr[n-1];
+    leaders.push_back(maxRight);
+
+    for(int i=n-2; i>=0;--i){
+        if(arr[i]>maxRight){
+        maxRight=arr[i];
+        leaders.push_back(maxRight);
+        }
+    }
+    reverse(leaders.begin(), leaders.end());
+    return leaders;
+    
+}
+int main(){
+    vector<int>arr={16,17,4,3,5,2};
+    vector<int>leaders=findLeaders(arr);
+    
+    cout<<"leaders:";
+    for(int num: leaders){
+        cout<<num<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
